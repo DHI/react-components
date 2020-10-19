08882053ccebb60fc943eae0599f73f0baf168f0
Graham <grahamoneale@gmail.com>
2020-10-19 18:13:41 +1000

    Exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
+export * from './Accounts/Accounts';
 export { default } from './Auth/AuthService';
 export * from './Auth/Login/Login';
 export * from './Auth/Login/types';
 export * from './Auth/LoginGate';
-export * from './Accounts/Accounts';
+export * from './ColourScales/ColourScale';
+export * from './ColourScales/ColourScaleHelper';
+export * from './ColourScales/types';
 export * from './common/Metadata/Metadata';
-export * from './UserGroups/UserGroups';
 export * from './DataServices/DataServices';
 export * from './DataServices/types';
 export * from './Jobs/JobList/JobList';
@@ -23,4 +25,6 @@ export * from './Scenarios/types';
 export * from './Timeseries/ChartPlotly/ChartPlotly';
 export * from './Timeseries/ChartPlotly/types';
 export * from './Timeseries/TimeseriesExporter/TimeseriesExporter';
+export * from './UserGroups/UserGroups';
 export * from './utils/Utils';
+
