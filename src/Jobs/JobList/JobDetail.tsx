8af503296226fb7e658d564904bc030e7b037915
Jean Loureiro <jlou@dhigroup.com>
2020-11-05 16:14:48 +1000

    Implement textarea scroll toggle

--- src/Jobs/JobList/JobDetail.tsx
+++ src/Jobs/JobList/JobDetail.tsx
@@ -1,50 +1,12 @@
-import { Grid, makeStyles, Paper, Theme, Typography } from '@material-ui/core';
+import { Grid, Paper, Typography } from '@material-ui/core';
 import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { setUtcToZonedTime } from '../../utils/Utils';
+import { JobDetailStyles } from './styles';
 import { JobDetailProps } from './types';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    flexGrow: 1,
-    height: '100%',
-  },
-  paper: {
-    display: 'flex',
-    padding: '16px 0',
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    justifyContent: 'space-evenly',
-    position: 'relative',
-    width: '100%',
-    alignItems: 'center',
-  },
-  item: {
-    display: 'flex',
-    flexDirection: 'column',
-    fontFamily: theme.typography.fontFamily,
-    fontSize: 12,
-  },
-  button: {
-    backgroundColor: 'transparent',
-    border: 'none',
-    position: 'absolute',
-    right: '0',
-    cursor: 'pointer',
-  },
-  textarea: {
-    width: '100%',
-    minHeight: 400,
-    overflow: 'scroll',
-    whiteSpace: 'nowrap',
-    height: 'calc(100% - 140px)',
-    marginTop: theme.spacing(2),
-  },
-}));
-
 const JobDetail = ({ detail, textareaScrolled, timeZone, dateTimeFormat, onClose }: JobDetailProps) => {
-  const classes = useStyles();
-  const textareaInput = useRef(null);
+  const classes = JobDetailStyles();
   const [structuredLogs, setStructuredLogs] = useState('');
   const [scrollHeight, setScrollHeight] = useState(null)
 
@@ -81,25 +43,16 @@ const JobDetail = ({ detail, textareaScrolled, timeZone, dateTimeFormat, onClose
     formatLog();
   }, [detail.logs]);
 
-  useEffect(() => {
-    if (textareaScrolled && textareaInput.current) {
-      textareaInput.current.scrollTop = textareaInput.current.scrollHeight;
-    }
-    // console.log('render');
-    // console.log(textareaInput.current);
-    // console.log(textareaInput.current.scrollHeight);
-
-
-  }, [textareaScrolled, detail.logs])
-
   const textareaInputRef = useCallback(node => {
-    console.log('render');
-    if (node !== null && textareaScrolled) {
-      setScrollHeight(node.scrollHeight)
-      node.scrollTop = scrollHeight;
+    if (node !== null) {
+      if (textareaScrolled) {
+        setScrollHeight(node.scrollHeight)
+        node.scrollTop = scrollHeight;
+      } else {
+        node.scrollTop = 0;
+      }
     }
-    console.log(scrollHeight)
-  }, [textareaScrolled, detail.logs])
+  }, [textareaScrolled, scrollHeight])
 
   return (
     <div className={classes.root}>
@@ -115,7 +68,7 @@ const JobDetail = ({ detail, textareaScrolled, timeZone, dateTimeFormat, onClose
       </Grid>
       {displayBlock(detail)}
 
-      <textarea name={detail.id} placeholder="" ref={textareaInputRef} className={classes.textarea} defaultValue={structuredLogs} />
+      <textarea name={detail.id} placeholder="" ref={(node) => textareaInputRef(node)} className={classes.textarea} defaultValue={structuredLogs} />
     </div>
   );
 };
--- src/Jobs/JobList/styles.ts
+++ src/Jobs/JobList/styles.ts
@@ -51,3 +51,42 @@ export const jobListTableStyles = (loadingDetail, tableBodyResponsive) =>
       height: '100vh',
     },
   }));
+
+
+export const JobDetailStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    height: '100%',
+  },
+  paper: {
+    display: 'flex',
+    padding: '16px 0',
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    justifyContent: 'space-evenly',
+    position: 'relative',
+    width: '100%',
+    alignItems: 'center',
+  },
+  item: {
+    display: 'flex',
+    flexDirection: 'column',
+    fontFamily: theme.typography.fontFamily,
+    fontSize: 12,
+  },
+  button: {
+    backgroundColor: 'transparent',
+    border: 'none',
+    position: 'absolute',
+    right: '0',
+    cursor: 'pointer',
+  },
+  textarea: {
+    width: '100%',
+    minHeight: 400,
+    overflow: 'scroll',
+    whiteSpace: 'nowrap',
+    height: 'calc(100% - 140px)',
+    marginTop: theme.spacing(2),
+  },
+}));
