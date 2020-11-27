315b0bb982fa29e28a36ca8ad93676b329f670bd
Jean Loureiro <jlou@dhigroup.com>
2020-11-27 16:00:40 +1000

    Fix MultiText default

--- src/common/DevExpress/MetadataEditor.tsx
+++ src/common/DevExpress/MetadataEditor.tsx
@@ -9,7 +9,7 @@ import {
   Select,
   Switch,
   TextField,
-  Typography,
+  Typography
 } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 import React, { Fragment, useEffect, useRef, useState } from 'react';
@@ -59,11 +59,15 @@ const MetadataEditor = ({ metadata, row, onChange, onListChange, onError }: Meta
 
   useEffect(() => {
     metadata.map((meta) => {
-      if (row.metadata && meta.type === 'MultiText') {
-        setList(row.metadata[meta.key] || []);
+      if (meta.type === 'MultiText') {
+        if (row.metadata) {
+          setList(row.metadata[meta.key] || []);
+        } else {
+          setList((meta.default as string[]) || []);
+        }
       }
     });
-  }, [row.metadata]);
+  }, [row]);
 
   return (
     <>
