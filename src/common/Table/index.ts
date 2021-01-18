b4d95dc9b01bc4f7145c1b5b3a13e2cc2812edad
jlou <jlou@dhigroup.com>
2021-01-18 13:22:35 +1000

    delete old table

--- src/common/Table/index.ts
+++
@@ -1,8 +0,0 @@
-import DefaultTable from './Table';
-import Dialog from './Dialog';
-import ActionsButtons from './ActionsButtons';
-import TopTableSection from './TableHeaders';
-import ActionsCell from './Cells/ActionsCell';
-import ChipCell, { MetadataChipCell } from './Cells/ChipCell';
-
-export { DefaultTable, Dialog, ActionsButtons, ActionsCell, ChipCell, MetadataChipCell, TopTableSection };
