e101f12d14c92cdf4e329e802e2547fb0a58efc3
jlou <jlou@dhigroup.com>
2021-02-08 17:00:03 +1000

    Move old scenario to a different folder

---
+++ src/Scenarios/ScenarioListOLD/ScenarioList.tsx
@@ -0,0 +1,135 @@
+import { Divider } from '@material-ui/core';
+import classNames from 'classnames';
+import { format, parseISO } from 'date-fns';
+import { Dictionary, groupBy, sortBy } from 'lodash';
+import React, { useEffect, useState } from 'react';
+import { checkCondition, checkStatus, getDescriptions, getObjectProperty, utcToTz } from '../../utils/Utils';
+import { ScenarioItem } from '../ScenarioItem/ScenarioItem';
+import { Scenario } from '../types';
+import ScenarioListProps from './types';
+import useStyles from './useStyles';
+
+const ScenarioList = (props: ScenarioListProps) => {
+  const {
+    selectedScenarioId,
+    scenarios,
+    showHour,
+    showDate,
+    menuItems,
+    status,
+    descriptionFields,
+    onContextMenuClick,
+    onScenarioSelected,
+    showStatus,
+    showMenu,
+    nameField,
+    timeZone,
+  } = props;
+  const [groupedScenarios, setGroupedScenarios] = useState<Dictionary<Scenario[]>>();
+  const [selectedId, setSelectedId] = useState(selectedScenarioId);
+  const classes = useStyles();
+
+  useEffect(() => {
+    groupScenarios(scenarios);
+  }, [scenarios]);
+
+  const groupScenarios = (scenarios: Scenario[]) => {
+    setGroupedScenarios(
+      showHour || showDate
+        ? groupBy(scenarios, (scenario) => {
+            return scenario.dateTime ? format(parseISO(scenario.dateTime), 'yyyy-MM-dd') : '';
+          })
+        : groupBy(scenarios, (scenario) => scenario.dateTime),
+    );
+  };
+
+  const buildMenu = (scenario: Scenario) => {
+    return menuItems.filter((menuItem) => {
+      return !menuItem.condition || checkCondition(scenario, menuItem.condition) ? menuItem : null;
+    });
+  };
+
+  const buildScenariosList = (scenarios: Scenario[]) => {
+    return sortBy(scenarios, ['dateTime'])
+      .reverse()
+      .map((scenario) => {
+        return (
+          <div
+            key={scenario.id}
+            onClick={() => onScenarioClick(scenario)}
+            onKeyPress={() => onScenarioClick(scenario)}
+            role="presentation"
+            className={classNames(classes.listItem, {
+              [classes.selectedItem]: selectedId === getObjectProperty(scenario, 'id'),
+            })}
+          >
+            <ScenarioItem
+              name={getObjectProperty(scenario.data, nameField)}
+              description={getDescriptions(scenario, descriptionFields, timeZone)}
+              date={showDate ? (scenario.dateTime ? scenario.dateTime.toString() : '') : null}
+              key={scenario.id}
+              isSelected={selectedId === getObjectProperty(scenario, 'id')}
+              onContextMenuClick={onContextMenuClick}
+              menu={buildMenu(scenario)}
+              showHour={showHour}
+              showMenu={showMenu}
+              showStatus={showStatus}
+              scenario={scenario}
+              status={checkStatus(scenario, status)}
+              timeZone={timeZone}
+            />
+          </div>
+        );
+      });
+  };
+
+  const buildDateArea = (date: string) => {
+    const isoDate = timeZone ? utcToTz(date, timeZone) : parseISO(date);
+    const dateObject = {
+      day: format(isoDate, 'dd'),
+      dayName: format(isoDate, 'EEE'),
+      monthName: format(isoDate, 'MMM'),
+    };
+    const dateBlockwidth = showHour ? '97px' : '39px';
+
+    return (
+      <div className={classes.dateBlock} style={{ width: dateBlockwidth }}>
+        <div className={classes.dateArea}>
+          <div className={classes.dayText}>{dateObject.day}</div>
+          <div className={classes.textFields}>{dateObject.monthName}</div>
+          <div className={classes.textFields}>{dateObject.dayName}</div>
+        </div>
+      </div>
+    );
+  };
+
+  const onScenarioClick = (scenario: Scenario) => {
+    if (scenario && selectedId !== getObjectProperty(scenario, 'id')) {
+      setSelectedId(getObjectProperty(scenario, 'id'));
+    }
+    if (onScenarioSelected) {
+      onScenarioSelected(scenario);
+    }
+  };
+
+  let printedScenarios = null;
+
+  if (groupedScenarios) {
+    printedScenarios = Object.keys(groupedScenarios)
+      .sort()
+      .reverse()
+      .map((key) => (
+        <div key={key}>
+          {showDate && buildDateArea(key)}
+          <div>
+            {buildScenariosList(groupedScenarios[key])}
+            <Divider variant="inset" className={classes.divider} />
+          </div>
+        </div>
+      ));
+  }
+
+  return <div className={classes.root}>{printedScenarios}</div>;
+};
+
+export { ScenarioListProps, ScenarioList };
---
+++ src/Scenarios/ScenariosOLD/types.ts
@@ -0,0 +1,157 @@
+import { JobParameters } from '../../DataServices/types';
+import { DescriptionField, MenuItem, QueryDates, Scenario, Status } from '../types';
+
+interface ScenariosProps {
+  /**
+   * Property field to specify the scenario name
+   */
+  nameField: string;
+  /**
+   * Indicates if the date of the scenario should be shown
+   */
+  showDate: boolean;
+  /**
+   * Indicates if the hour of the scenario should be shown
+   */
+  showHour: boolean;
+  /**
+   * Indicates if the menu options of the scenario should be shown
+   */
+  showMenu: boolean;
+  /**
+   * Indicates if the status of the scenario should be shown
+   */
+  showStatus: boolean;
+  /**
+   * The id to execute scenario
+   */
+  taskId: string;
+  /**
+   * The hostgroup used to execute the job
+   */
+  hostGroup?: string;
+  /**
+   * The id of selected scenario when it clicked
+   */
+  selectedScenarioId?: string;
+  /**
+   * Time interval to fetch data in second
+   */
+  frequency?: number;
+  /**
+   * Object to filter data by its property
+   * @param field object property e.g.: data.mooring
+   * @param value to filter to a specific value
+   */
+  dataFilterbyProperty?: {
+    field: string;
+    value?: string | null;
+  };
+  /**
+   * Backend host
+   */
+  host: string;
+  /**
+   * Authorization header to backend call
+   */
+  token: string;
+  /**
+   * Scenario connection to fetch scenario by date
+   */
+  scenarioConnection: string;
+  /**
+   * Connection to fetch data from backend
+   */
+  jobConnection: string;
+  /**
+   * Set of job parameters
+   */
+  jobParameters?: JobParameters;
+  /**
+   * Temporary for discarding modules which do not match
+   */
+  module?: string;
+  /**
+   * Value range to fetch scenario by date
+   */
+  queryDates?: QueryDates;
+  /**
+   * Customising translation dialog
+   */
+  translations?: {
+    /**
+     * Scenario termination confirmation dialog
+     */
+    terminateConfirmation?: string;
+    /**
+     * Scenario execution confirmation dialog
+     */
+    executeConfirmation?: string;
+    /**
+     * Scenario clone Title dialog
+     */
+    cloneTitle?: string;
+    /**
+     * Scenario clone confirmation dialog
+     */
+    cloneConfirmation?: string;
+    /**
+     * Scenario delete title dialog
+     */
+    deleteTitle?: string;
+    /**
+     * Scenario deletion confirmation dialog
+     */
+    deleteConfirmation?: string;
+    /**
+     * Value to be add as a cancel button text
+     */
+    cancelLabel?: string;
+    /**
+     * Value to be add as a confirm button text
+     */
+    confirmLabel?: string;
+  };
+  /**
+   * Customising scenario status display
+   */
+  status: Status[];
+  /**
+   * Customising scenario description field display
+   */
+  descriptionFields?: DescriptionField[];
+  /**
+   * Customising scenario extra field display
+   */
+  extraFields?: DescriptionField[];
+  /**
+   * The scenario menu options
+   */
+  menuItems: MenuItem[];
+  /**
+   * The scenario menu function handlers
+   */
+  onContextMenuClick: (menuItem: MenuItem, scenario: Scenario) => void;
+  /**
+   * Emit event to client when scenario selected by user (basic scenario data)
+   */
+  onScenarioSelected: (scenario: Scenario) => void;
+  /**
+   * Emit event to client when scenario received from the server after a selection (full scenario data)
+   */
+  onScenarioReceived: (scenario: Scenario) => void;
+  /**
+   * Emit even to client when list of scenarios received from the server
+   */
+  onScenariosReceived: (scenarios: Scenario[]) => void;
+  /**
+   * The object data to be added as new scenario (optional)
+   */
+  addScenario?: Scenario | undefined;
+  /**
+   * Time zone (IANA format) for date display
+   */
+  timeZone?: string;
+}
+
+export default ScenariosProps;
