0e3dec81f5b7348211000b2543d8efb0ada97554
Razvan Bertea <30385700+bertearazvan@users.noreply.github.com>
2021-03-10 16:04:41 +0100

    trigger small change on react-components

--- packages/react-components/.prettierrc.js
+++ packages/react-components/.prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ...require('eslint-config-standard-typescript-prettier/prettier'),
     printWidth: 120,
-    useTabs: false,
+    useTabs: true,
     semi: true,
     singleQuote: true,
     arrowParens: 'always',
