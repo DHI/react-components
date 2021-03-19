9298e0c92355cfb903379355e723b7855abefbc7
Razvan <rabe@dhigroup.com>
2021-03-19 16:10:33 +0100

    trigger pipe2

--- packages/react-components-lab/.eslintrc.js
+++ packages/react-components-lab/.eslintrc.js
@@ -14,6 +14,6 @@ module.exports = {
   },
   rules: {
     'react/prop-types': 'off',
-    'no-multiple-empty-lines': ['warn', { max: 3 }],
+    'no-multiple-empty-lines': ['warn', { max: 2 }],
   },
 };
