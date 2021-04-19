bda6fdffeb189a5cd61a506f8a08e1bb581090da
rosudhi <rosu@dhigroup.com>
2021-04-19 08:06:06 +0700

    Use TypographyOptions type to typography

--- packages/react-components-lab/src/components/ThemeProvider/mikeTypography.ts
+++ packages/react-components-lab/src/components/ThemeProvider/mikeTypography.ts
@@ -1,6 +1,7 @@
+import { TypographyOptions } from '@material-ui/core/styles/createTypography';
 import { HTML_FONT_SIZE, FONT_SIZE, FONT_FAMILY } from './types';
 
-const mikeTypography = {
+const mikeTypography: TypographyOptions = {
   htmlFontSize: HTML_FONT_SIZE,
   fontSize: FONT_SIZE,
   fontFamily: FONT_FAMILY,
