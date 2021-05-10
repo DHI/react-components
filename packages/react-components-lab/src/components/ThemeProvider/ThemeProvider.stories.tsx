701e13b9981892b24ec28d6755b313d96fed979e
Ronald Sutantio <rosu@dhigroup.com>
2021-05-10 16:46:44 +0700

    Add layout style for themes component

--- packages/react-components-lab/src/components/ThemeProvider/ThemeProvider.stories.tsx
+++ packages/react-components-lab/src/components/ThemeProvider/ThemeProvider.stories.tsx
@@ -1,14 +1,18 @@
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Meta, Story } from '@storybook/react/types-6-0';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@material-ui/core/Box';
-import { Typography, Switch } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 
 // #region Local imports
 import ThemeProvider from './ThemeProvider';
 import { IProps } from './types';
-import ComponentsMui from './ComponentsMui';
+import ComponentsMui from './ComponentsMui/ComponentsMui';
+import Syntax from '../Syntax/Syntax';
+import jsonData from './ComponentsMui/components-mui-data.json';
+import { ComponentList } from './ComponentsMui/types';
+import useStyles from './styles';
 // #endregion
 
 export default {
@@ -24,26 +28,81 @@ export default {
   },
 } as Meta;
 
-const Template: Story<IProps> = (args) => (
-  <ThemeProvider {...args}>
-    <Box>
+const Template: Story<IProps> = (args) => {
+  const classes = useStyles();
+
+  const [componentDataList, setComponentDataList] = useState<ComponentList[]>();
+
+  useEffect(() => {
+    const newList: ComponentList[] = [];
+    setComponentDataList(Object.assign(newList, jsonData));
+  }, []);
+
+  return (
+    <ThemeProvider {...args}>
+      <div className={classes.root}>
+        <Typography variant="h1">Theme Provider</Typography>
+        <div className={classes.header}>
+          <Typography variant="h4">
+            <span className={classes.highlightText}>Theme Provider</span> is
+            theming that built on top of Material-Ui styles and overridden based
+            on DHI official design guidelines styles. Here is the concept.
+          </Typography>
+        </div>
+        <main className={classes.mainContainer}>
+          <div className={classes.content}>
+            <Box bgcolor="primary.main" margin={1} padding={2}>
+              <Typography variant="h2" color="textSecondary">
+                Theme Provider
+              </Typography>
+              <Box bgcolor="primary.light" margin={1} padding={2}>
+                <Typography variant="body1">Your Application</Typography>
+                <Box bgcolor="primary.main" margin={1} padding={2}>
+                  <Typography variant="body1" color="textSecondary">
+                    Your components
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+            <Syntax
+              code={`<ThemeProvider>
+  <Box bgcolor="primary.main" margin={1} padding={2}>
+    <Typography variant="h2" color="textSecondary">
+      Theme Provider
+    </Typography>
+    <Box bgcolor="primary.light" margin={1} padding={2}>
+      <Typography variant="body1">Your Application</Typography>
       <Box bgcolor="primary.main" margin={1} padding={2}>
-        <Typography variant="h2" color="textSecondary">
-          Theme Provider
+        <Typography variant="body1" color="textSecondary">
+          Your components
         </Typography>
-        <Box bgcolor="primary.light" margin={1} padding={2}>
-          <Typography variant="body1">Your Application</Typography>
-          <Box bgcolor="primary.main" margin={1} padding={2}>
-            <Typography variant="body1" color="textSecondary">
-              Your components
-            </Typography>
-          </Box>
-        </Box>
       </Box>
-      <ComponentsMui />
     </Box>
-  </ThemeProvider>
-);
+  </Box>
+</ThemeProvider>`}
+            />
+          </div>
+          <div className={classes.sidenav}>
+            <div className={classes.component}>
+              <div className={classes.sidenavHeader}>
+                <Typography variant="h5">Contents</Typography>
+              </div>
+              <div className={classes.sidenavContent}>
+                <ul>
+                  {componentDataList &&
+                    componentDataList.map((item) => (
+                      <li className={classes.item}>{item.title}</li>
+                    ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <ComponentsMui dataList={componentDataList} />
+        </main>
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export const Primary = Template.bind({});
 Primary.args = {};
--- packages/react-components-lab/src/components/ThemeProvider/styles.ts
+++ packages/react-components-lab/src/components/ThemeProvider/styles.ts
@@ -1,32 +1,111 @@
+import { makeStyles } from '@material-ui/core/styles';
 import mikePalette from './mikePallete';
 
-const muiRadioStyles = {
-  background: 'transparent',
-  height: '20px',
-  width: '20px',
-  margin: '-4px 0px',
-  '& svg:nth-of-type(1)': {
-    transform: 'scale(0.9)',
-  },
-  '& svg:nth-of-type(1) > path': {
-    fill: mikePalette.darkGrey.main,
-  },
-  '&$disabled': {
-    '& svg:nth-of-type(1) > path': {
-      fill: mikePalette.darkGrey.light,
+const Styles = makeStyles((theme) => ({
+  root: {
+    marginLeft: '2em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
     },
   },
-  /*  JSS `:after` won't apply to target unless there is a valid value for content: */
-  '&:after': {
-    content: 'open-quote',
-    color: mikePalette.mediumGrey.light,
-    background: mikePalette.mediumGrey.light,
-    display: 'block',
-    height: '20px',
-    width: '20px',
-    marginLeft: '-20px',
-    borderRadius: '10px',
+  mainContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
   },
-};
+  header: {
+    display: 'flex',
+    width: 'calc(100% - 230px)',
+    paddingTop: 20,
+    paddingBottom: 15,
+    [theme.breakpoints.down('xs')]: {
+      width: '100%',
+    },
+  },
+  content: {
+    flexBasis: 0,
+    flexGrow: 300,
+    minWidth: '40%',
+    [theme.breakpoints.down('xs')]: {
+      minWidth: '100%',
+    },
+  },
+  sidenav: {
+    [`--offset`]: '2rem',
+    flexGrow: 1,
+    flexBasis: 230,
+    alignSelf: 'start',
+    position: 'sticky',
+    top: '2rem',
+  },
+  sidenavHeader: {},
+  sidenavContent: {},
+  item: {},
+  component: {
+    display: 'grid',
+    gridTemplateRows: 'auto 1fr auto',
+    position: 'relative',
+    padding: '10px 10px 10px 25px',
+    '& $sidenavHeader': {},
+    '& $sidenavContent': {
+      '& $ul': {
+        listStyle: 'none',
+        '& $li': {
+          marginLeft: -20,
+          paddingLeft: 10,
+          lineHeight: '2.2em',
+          '&:hover': {
+            color: mikePalette.primary.light,
+            borderLeft: `3px solid ${mikePalette.primary.light}`,
+            cursor: 'pointer',
+          },
+        },
+      },
+    },
+    [theme.breakpoints.down('xs')]: {
+      display: 'none',
+    },
+  },
+  highlightText: {
+    backgroundColor: mikePalette.primary.light,
+    borderRadius: 5,
+    padding: '2px 4px 2px 4px',
+  },
+  codeContainer: {
+    borderRadius: 5,
+    backgroundColor: '#000',
+  },
+}));
 
-export default muiRadioStyles;
+export default Styles;
+
+// const muiRadioStyles = {
+//   background: 'transparent',
+//   height: '20px',
+//   width: '20px',
+//   margin: '-4px 0px',
+//   '& svg:nth-of-type(1)': {
+//     transform: 'scale(0.9)',
+//   },
+//   '& svg:nth-of-type(1) > path': {
+//     fill: mikePalette.darkGrey.main,
+//   },
+//   '&$disabled': {
+//     '& svg:nth-of-type(1) > path': {
+//       fill: mikePalette.darkGrey.light,
+//     },
+//   },
+//   /*  JSS `:after` won't apply to target unless there is a valid value for content: */
+//   '&:after': {
+//     content: 'open-quote',
+//     color: mikePalette.mediumGrey.light,
+//     background: mikePalette.mediumGrey.light,
+//     display: 'block',
+//     height: '20px',
+//     width: '20px',
+//     marginLeft: '-20px',
+//     borderRadius: '10px',
+//   },
+// };
+
+// export default muiRadioStyles;
