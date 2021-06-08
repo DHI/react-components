5fe10a6fece712488d2a74e669041048d6d98eb5
Ronald Sutantio <rosu@dhigroup.com>
2021-06-08 15:53:00 +0700

    Remove sidenav styles and move it into it's component's folder

--- packages/react-components-lab/src/components/ThemeProvider/styles.ts
+++ packages/react-components-lab/src/components/ThemeProvider/styles.ts
@@ -1,5 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles';
+
+// #region Local imports
 import mikePalette from './mikePallete';
+// #endregion
 
 const Styles = makeStyles((theme) => ({
   root: {
@@ -30,42 +33,6 @@ const Styles = makeStyles((theme) => ({
       minWidth: '100%',
     },
   },
-  sidenav: {
-    [`--offset`]: '2rem',
-    flexGrow: 1,
-    flexBasis: 230,
-    alignSelf: 'start',
-    position: 'sticky',
-    top: '2rem',
-  },
-  sidenavHeader: {},
-  sidenavContent: {},
-  item: {},
-  component: {
-    display: 'grid',
-    gridTemplateRows: 'auto 1fr auto',
-    position: 'relative',
-    padding: '10px 10px 10px 25px',
-    '& $sidenavHeader': {},
-    '& $sidenavContent': {
-      '& $ul': {
-        listStyle: 'none',
-        '& $li': {
-          marginLeft: -20,
-          paddingLeft: 10,
-          lineHeight: '2.2em',
-          '&:hover': {
-            color: mikePalette.primary.light,
-            borderLeft: `3px solid ${mikePalette.primary.light}`,
-            cursor: 'pointer',
-          },
-        },
-      },
-    },
-    [theme.breakpoints.down('xs')]: {
-      display: 'none',
-    },
-  },
   highlightText: {
     backgroundColor: mikePalette.primary.light,
     borderRadius: 5,
@@ -74,11 +41,6 @@ const Styles = makeStyles((theme) => ({
   codeContainer: {
     borderRadius: 5,
     backgroundColor: '#000',
-  },
-  tabItem: {
-    textDecoration: 'none',
-    cursor: 'pointer',
-    color: 'inherit',
   },
 }));
 
