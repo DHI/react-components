3ecdce721b3dd19c76c3ec6402145865113a99ed
Brigitta Roberta Rücz <brir@dhigroup.com>
2021-07-05 16:28:10 +0200

    Add useLocalStorage to hooks

---
+++ packages/react-components-lab/src/hooks/useLocalStorage/types.ts
@@ -0,0 +1,9 @@
+interface InitialVal {
+  initialValue: unknown;
+}
+
+interface HookTypes extends InitialVal {
+  key: string;
+}
+
+export { InitialVal, HookTypes };
---
+++ packages/react-components-lab/src/hooks/useLocalStorage/useLocalStorage.ts
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { HookTypes } from './types';
+
+export default ({
+  key,
+  initialValue,
+}: HookTypes): [unknown, (value: unknown) => void] => {
+  const [storageValue, setStorageValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+
+    return item ? String(JSON.parse(item)) : initialValue;
+  });
+
+  const setValue = (value: unknown) => {
+    setStorageValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storageValue, setValue];
+};
