5a85643ef67e0ce0c70b45b86d596cd09285752e
rira <rira@dhigroup.com>
2021-07-07 13:48:56 +0200

    perf(icons): Release

--- packages/icons/.eslintrc.js
+++ packages/icons/.eslintrc.js
@@ -6,28 +6,17 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    // 'airbnb-typescript',
-    // 'plugin:@typescript-eslint/recommended',
-    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:eslint-comments/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
     'prettier',
   ],
-  // parser: '@typescript-eslint/parser',
-  // parserOptions: {
-  //   ecmaVersion: 2019,
-  //   project: 'tsconfig.json',
-  //   // tsconfigRootDir: __dirname,
-  // },
   rules: {
     'react/prop-types': 'off',
     'react/require-default-props': 'off',
     'react/jsx-props-no-spreading': 'off',
     'no-multiple-empty-lines': ['warn', { max: 2 }],
     'prettier/prettier': 'error',
-    // '@typescript-eslint/no-unsafe-assignment': 'warn',
-    // '@typescript-eslint/no-unsafe-return': 'warn',
   },
 };
