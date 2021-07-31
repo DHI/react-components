3c1df16f53cbd43cd30d93a9e020d507a61fc0b5
Ronald Sutantio <rosu@dhigroup.com>
2021-07-31 09:15:30 +0700

    Bump storybook to latest version (6.3.6)

--- packages/react-components-lab/.storybook/preview.js
+++ packages/react-components-lab/.storybook/preview.js
@@ -8,4 +8,8 @@ addDecorator((story) => (
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
 }
