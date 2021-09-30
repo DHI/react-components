6788849c4f03e09e608cce98934e731a6108d165
Razvan <rabe@dhigroup.com>
2021-09-30 15:25:55 +0200

    add CollapsableBox component

---
+++ packages/react-components-lab/src/components/CollapsableBox/CollapsableBox.stories.tsx
@@ -0,0 +1,38 @@
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Meta, Story } from '@storybook/react/types-6-0'; // eslint-disable-line import/no-extraneous-dependencies
+import { Typography, Box } from '@material-ui/core';
+import React from 'react';
+import CollapsableBox from './CollapsableBox';
+import { CollapsableBoxProps } from './types';
+
+export default {
+  title: 'Example/CollapsableBox',
+  component: CollapsableBox,
+} as Meta;
+
+const Template: Story<CollapsableBoxProps> = (args) => (
+  <Box width={1} display="flex" justifyContent="center">
+    <CollapsableBox {...args}>
+      <Typography variant="body2" gutterBottom>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Typography>
+    </CollapsableBox>
+  </Box>
+);
+
+export const AllProps = Template.bind({});
+AllProps.args = {
+  collapseLabel: 'Collapse',
+  expandLabel: 'Expand',
+  style: {
+    width: 300,
+  },
+};
---
+++ packages/react-components-lab/src/components/CollapsableBox/styles.ts
@@ -0,0 +1,28 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+  collapsable: {
+    overflow: 'hidden',
+    position: 'relative',
+  },
+  overlay: {
+    position: 'absolute',
+    opacity: 0,
+    pointerEvents: 'none',
+    width: '100%',
+    bottom: 0,
+    background:
+      'linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%)',
+  },
+  collapsedOverlay: {
+    height: 50,
+    opacity: 1,
+  },
+  icon: {
+    transform: 'rotateX(180deg)',
+  },
+  button: {
+    height: 'unset',
+    padding: 'unset',
+  },
+});
