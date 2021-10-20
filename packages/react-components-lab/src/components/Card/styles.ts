74afe4a182331a316c1faf027b4a4c78011bde8a
Razvan <rabe@dhigroup.com>
2021-10-20 14:57:39 +0200

    fix dependency cycle

--- packages/react-components-lab/src/components/Card/styles.ts
+++ packages/react-components-lab/src/components/Card/styles.ts
@@ -1,5 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { IMikeTheme } from '../..';
+import { IMikeTheme } from '../ThemeProvider/types';
 
 export default makeStyles<IMikeTheme>((theme) => ({
   root: {
