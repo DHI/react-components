7f4301d1aa1ba5383ea5f8c0c247c7faf1aba8e8
Razvan <rabe@dhigroup.com>
2021-10-20 15:15:11 +0200

    remove webpack custom config

--- packages/react-components-lab/.storybook/main.js
+++ packages/react-components-lab/.storybook/main.js
@@ -13,19 +13,4 @@ module.exports = {
     reactDocgen: 'react-docgen-typescript',
     tsconfigPath: "./../tsconfig.json"
   },
-  webpackFinal: async config => {
-    config.devtool = 'source-map'
-
-    config.module.rules.push({
-      test: /[.]stories[.]tsx?$/,
-      use: [
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
-        },
-      ],
-      enforce: 'pre',
-    });
-
-    return config;
-  },
 }
