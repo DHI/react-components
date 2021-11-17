2d20aa03fe74d150235afba5720c551e2b538d35
rira <rira@dhigroup.com>
2021-11-17 15:29:38 +0100

    Add MultiField

---
+++ packages/react-components-lab/src/components/MultiField/MultiField.stories.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Story } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
+import { Typography, Box } from '@material-ui/core';
+import MultiField from './MultiField';
+import { MultiFieldProps } from './types';
+
+export default {
+  title: 'Example/MultiField',
+  component: MultiField,
+};
+
+const Template: Story<MultiFieldProps> = (args) => {
+  const [value, setValue] = useState<string | undefined>();
+
+  console.log(value);
+
+  return (
+    <>
+      <Typography>MultiField</Typography>
+      <Box
+        position="inline"
+        height={500}
+        width={600}
+        p={2}
+        m={2}
+        style={{ backgroundColor: '#cccccc' }}
+      >
+        <Typography>Parent view</Typography>
+        <MultiField
+          {...args}
+          value={value}
+          length={6}
+          seperatorChar="-"
+          seperationInterval={3}
+          onChange={(newValue) => setValue(newValue)}
+        />
+      </Box>
+    </>
+  );
+};
+
+export const Minimal = Template.bind({});
+Minimal.args = {};
