f6b2c2f22cbc9836ddd32c50ccfc4522cd7a65a0
Razvan <rabe@dhigroup.com>
2022-01-05 14:30:53 +0100

    turn defaultprops rule off

--- packages/react-components-lab/.eslintrc.js
+++ packages/react-components-lab/.eslintrc.js
@@ -33,5 +33,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/naming-convention': 'off',
     'import/no-extraneous-dependencies': 'off',
+    'react/default-props-match-prop-types': 'off'
   },
 };
