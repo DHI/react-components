de78e38f0c9832ef0d0cafefeb207b6459dda78c
Razvan <rabe@dhigroup.com>
2022-01-05 12:30:58 +0100

    feat(CalendarItem): Add CalendarItem component

---
+++ packages/react-components-lab/src/components/CalendarItem/CalendarItem.tsx
@@ -0,0 +1,36 @@
+import React, { FC, ReactNode } from 'react';
+import { Box, Typography } from '@material-ui/core';
+import clsx from 'clsx';
+import useStyles from './styles';
+import { CalendarItemProps } from './types';
+
+const CalendarItem: FC<CalendarItemProps> = ({
+  active = false,
+  variant,
+  onClick,
+  children,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Box
+      className={clsx(
+        variant === 'button' && {
+          [classes.button]: true,
+          [classes.active]: active,
+        },
+        variant === 'semi-button' && {
+          [classes.button]: true,
+          [classes.semiActive]: active,
+        }
+      )}
+      onClick={onClick}
+    >
+      <Typography variant="body2" style={{ color: 'inherit' }}>
+        {children}
+      </Typography>
+    </Box>
+  );
+};
+
+export default CalendarItem;
