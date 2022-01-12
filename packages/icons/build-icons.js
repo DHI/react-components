58207b6abad56b318e82ba80cae9036cb01cdd18
Razvan <rabe@dhigroup.com>
2022-01-12 14:21:59 +0100

    change to object mapping

--- packages/icons/build-icons.js
+++ packages/icons/build-icons.js
@@ -10,12 +10,14 @@ const traverse = require('traverse');
 const distDir = './dist/';
 const srcDir = './src/';
 
-const colorPrimary = '#0B4566';
-const colorWhite = '#FFF';
-const colorError = '#FD3F75';
-const colorWarning = '#FFC20A';
-const colorSecondary = '#00A4EC';
-const colorDarkGreyMain = '#86A2B3';
+const colorMap = {
+  colorPrimary: '#0B4566',
+  colorSecondary: '#00A4EC',
+  colorDarkGreyMain: '#86A2B3',
+  colorWhite: '#FFF',
+  colorError: '#FD3F75',
+  colorWarning: '#FFC20A',
+};
 
 const template = ({ template }, opts, { imports, componentName, jsx }) =>
   template.smart({ plugins: ['typescript'] }).ast`
@@ -43,12 +45,21 @@ for (const svgFile of svgFiles) {
   const { svg: svgObj } = JSON.parse(xmlParser.toJson(svg));
 
   const updatedSVG = traverse(svgObj).map(function (x) {
-    if (this.isLeaf && x === 'colorPrimary') this.update(colorPrimary);
-    if (this.isLeaf && x === 'colorSecondary') this.update(colorSecondary);
-    if (this.isLeaf && x === 'colorWhite') this.update(colorWhite);
-    if (this.isLeaf && x === 'colorError') this.update(colorError);
-    if (this.isLeaf && x === 'colorWarning') this.update(colorWarning);
-    if (this.isLeaf && x === 'colorDarkGreyMain') this.update(colorDarkGreyMain);
+    if (this.isLeaf && x === 'colorPrimary')
+      this.update(colorMap[colorPrimary]);
+
+    if (this.isLeaf && x === 'colorSecondary')
+      this.update(colorMap[colorSecondary]);
+
+    if (this.isLeaf && x === 'colorWhite') this.update(colorMap[colorWhite]);
+
+    if (this.isLeaf && x === 'colorError') this.update(colorMap[colorError]);
+
+    if (this.isLeaf && x === 'colorWarning')
+      this.update(colorMap[colorWarning]);
+
+    if (this.isLeaf && x === 'colorDarkGreyMain')
+      this.update(colorMap[colorDarkGreyMain]);
   });
   const newSvg = xmlParser.toXml(JSON.stringify({ svg: { ...updatedSVG } }));
   const height = svgObj.height ?? 40;
