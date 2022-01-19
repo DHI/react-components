503b2e0c8be69ac3182c3d1fbd928d71a3ddaf22
Razvan <rabe@dhigroup.com>
2022-01-19 16:15:07 +0100

    fix linting in eslint file

--- packages/react-components-lab/.eslintrc.js
+++ packages/react-components-lab/.eslintrc.js
@@ -1,19 +1,32 @@
 module.exports = {
   ignorePatterns: ['node_modules', 'dist', 'storybook-static'],
   plugins: ['prettier', 'eslint-comments', 'import', '@typescript-eslint'],
-  extends: ["airbnb-typescript", "plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended-requiring-type-checking", "plugin:eslint-comments/recommended", "plugin:import/errors", "plugin:import/warnings", "plugin:import/typescript", "prettier", "plugin:storybook/recommended"],
+  extends: [
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:eslint-comments/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+    'plugin:storybook/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2019,
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
   rules: {
     'react/prop-types': 'off',
     'react/require-default-props': 'off',
     'react/jsx-props-no-spreading': 'off',
-    'no-multiple-empty-lines': ['warn', {
-      max: 2
-    }],
+    'no-multiple-empty-lines': [
+      'warn',
+      {
+        max: 2,
+      },
+    ],
     'prettier/prettier': 'error',
     // Rules disabled for lab
     'no-console': 'off',
@@ -24,6 +37,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/naming-convention': 'off',
     'import/no-extraneous-dependencies': 'off',
-    'react/default-props-match-prop-types': 'off'
-  }
+    'react/default-props-match-prop-types': 'off',
+  },
 };
