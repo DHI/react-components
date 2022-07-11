ba4a44d3f87bd8779492ca493c1fc011a1e72c19
DenizYil <denizyil@outlook.dk>
2022-07-11 03:43:26 +0200

    t

---
+++ packages/icons/src/Text.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import createSvgIcon from "../utils/createSvgIcon";
+
+
+export default createSvgIcon(
+    <path fill="#09334B" d="M2 0H0V3H2V0Z M12 0H10V3H12V0Z M4 12V14H8V12H4Z M2 0L1 2H11L10 0H2Z M5 0H7V12H5V0Z"></path>,
+    'Text'
+)
---
+++ packages/icons/src/Unlock.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import createSvgIcon from "../utils/createSvgIcon";
+
+
+throw new Error("NO COPY")
+export default createSvgIcon(
+    <path></path>,
+    'Unlock'
+)
+
