d5048d0de9caae33a1962df37b7377a75292d082
DenizYil <denizyil@outlook.dk>
2022-07-12 10:55:46 +0200

    refactor: update icons story to use the new icons

--- packages/react-components-lab/src/icons/Icons.tsx
+++ packages/react-components-lab/src/icons/Icons.tsx
@@ -1,8 +1,22 @@
-import React, { FC, createElement, useState } from 'react';
-import { Box, Typography, Tooltip } from '@material-ui/core';
-import * as DhiIcons from '@dhi/icons/dist'; // eslint-disable-line import/no-unresolved
+import React, { FC, useState } from 'react';
+import { Box, Typography, Tooltip, SvgIcon, Grid } from '@material-ui/core';
+import * as DhiIcons from '@dhi/icons/dist';
 import copy from 'copy-to-clipboard';
 import useStyles from './styles';
+
+type ModuleType = typeof DhiIcons;
+
+export function getIcon<K extends keyof ModuleType>(
+  input: K | string
+): typeof SvgIcon {
+  const foundIcon = Object.keys(DhiIcons).find((key) => key === input);
+  return DhiIcons[foundIcon as K];
+}
+
+const DynamicIcon: FC<{ name: string }> = ({ name }) => {
+  const IconComponent = getIcon(name);
+  return <IconComponent />;
+};
 
 const Icons: FC = () => {
   const [tooltipText, showTooltipText] = useState<string | undefined>(
@@ -19,41 +33,29 @@ const Icons: FC = () => {
   };
 
   return (
-    <Box
-      flexWrap="wrap"
-      width={1}
-      className={classes.root}
-      display="flex"
-      justifyContent="flex-start"
-      p={1}
-    >
+    <Grid container>
       {Object.keys(DhiIconsTyped)
         .sort()
         .map((item) => (
-          <Tooltip
-            open={Boolean(tooltipText && tooltipText === item)}
-            title={`Copied ${item} to clipboard.`}
-          >
-            <Box
-              onClick={() => handleIcon(item)}
-              className={classes.iconWrapper}
-              m={0.5}
-              p={1}
-              width={130}
-              height={70}
-              display="flex"
-              flexDirection="column"
+          <Grid item xs={2}>
+            <Tooltip
+              key={item}
+              open={Boolean(tooltipText && tooltipText === item)}
+              title={`Copied ${item} to clipboard.`}
             >
-              <Box height={1} display="flex" justifyContent="center">
-                {createElement(DhiIconsTyped[item])}
+              <Box
+                onClick={() => handleIcon(item)}
+                className={classes.iconWrapper}
+              >
+                <DynamicIcon name={item} />
+                <Typography variant="body2" className={classes.iconText}>
+                  {item}
+                </Typography>
               </Box>
-              <Typography variant="body2" className={classes.iconText}>
-                {item}
-              </Typography>
-            </Box>
-          </Tooltip>
+            </Tooltip>
+          </Grid>
         ))}
-    </Box>
+    </Grid>
   );
 };
 
