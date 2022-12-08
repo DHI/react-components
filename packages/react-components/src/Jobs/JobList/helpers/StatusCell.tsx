14fa07fa3bc925d8bbf34b37cd68bcf40a7e1291
sude-dewi <118501050+sude-dewi@users.noreply.github.com>
2022-12-08 11:27:15 +0700

    useMemo

--- packages/react-components/src/Jobs/JobList/helpers/StatusCell.tsx
+++ packages/react-components/src/Jobs/JobList/helpers/StatusCell.tsx
@@ -1,70 +1,76 @@
 import { Box, CircularProgress, Tooltip, Typography } from '@material-ui/core';
 import { blue, green, red, yellow } from '@material-ui/core/colors';
 import { Cancel, CancelScheduleSend, CheckCircle, Error, HelpOutline, HourglassEmpty } from '@material-ui/icons';
-import React from 'react';
+import is from 'date-fns/esm/locale/is/index.js';
+import React, { useMemo } from 'react';
 
 const StatusCell = ({ row }: { row: any }) => {
   const { status, progress } = row;
-
+  
+  
+    console.log(status, progress );
+    return useMemo(()=>{
   switch (status) {
     case 'Completed':
       return (
         <Tooltip title={status}>
-          <CheckCircle style={{ color: green[300] }} />
-        </Tooltip>
+        <CheckCircle style={{ color: green[300] }} />
+      </Tooltip>
       );
     case 'InProgress':
-      return (
+      // return useMemo(()=>(
+        return(
         <Tooltip title={status}>
-          <Box position="relative" display="inline-flex">
-            <CircularProgress style={{ color: blue[900] }} variant={'indeterminate'} size={28} thickness={4} />
-            <Box
-              top={0}
-              left={0}
-              bottom={0}
-              right={0}
-              position="absolute"
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-            >
-              <Typography variant="caption" component="div" style={{ fontSize: 10 }}>{`${progress}%`}</Typography>
-            </Box>
+        <Box position="relative" display="inline-flex">
+          <CircularProgress style={{ color: blue[900] }} variant={'indeterminate'} size={28} thickness={4} />
+          <Box
+            top={0}
+            left={0}
+            bottom={0}
+            right={0}
+            position="absolute"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography variant="caption" component="div" style={{ fontSize: 10 }}>{`${progress}%`}</Typography>
           </Box>
-        </Tooltip>
-      );
+        </Box>
+      </Tooltip>
+      // ),[status,progress]);
+        );
     case 'Pending':
       return (
         <Tooltip title={status}>
-          <HourglassEmpty style={{ color: yellow[900] }} />
-        </Tooltip>
+        <HourglassEmpty style={{ color: yellow[900] }} />
+      </Tooltip>
       );
     case 'Error':
       return (
         <Tooltip title={status}>
-          <Error style={{ color: red[300] }} />
+        <Error style={{ color: red[300] }} />
         </Tooltip>
       );
     case 'Cancelled':
       return (
         <Tooltip title={status}>
-          <Cancel style={{ color: yellow[900] }} />
-        </Tooltip>
+        <Cancel style={{ color: yellow[900] }} />
+      </Tooltip>
       );
     case 'Cancelling':
     case 'Cancel':
       return (
         <Tooltip title={status}>
-          <CancelScheduleSend style={{ color: yellow[900] }} />
-        </Tooltip>
+        <CancelScheduleSend style={{ color: yellow[900] }} />
+      </Tooltip>
       );
     default:
       return (
         <Tooltip title={status}>
-          <HelpOutline style={{ color: yellow[900] }} />
-        </Tooltip>
+      <HelpOutline style={{ color: yellow[900] }} />
+    </Tooltip>
       );
-  }
+  }},[status,progress])
 };
 
 export default StatusCell;
